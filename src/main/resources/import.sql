3fecc6c9163e88f70b6b450934497eadc7c4adf5
AlejandroOlea <alejandroolea58mail.com>
2021-01-29 19:56:22 -0600

    Contiene todas las funciones basicas de un CRUD para la entidad Clientes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,26 @@
+/* poblando tables */
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(2, 'Frankie', 'De John', 'dejohn@gmail.com', '2017-08-28', '');
+
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(3, 'Andres', 'Guzman', 'mini@bolsas.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(4, 'Frankie', 'De John', 'dedjohn@gmanil.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(5, 'Andres', 'Guzman', 'profedsor@bolsma.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(6, 'Frankie', 'De John', 'dejochn@gmaiul.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(7, 'Andres', 'Guzman', 'profescaocr@bolsua.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(8, 'Frankie', 'De John', 'dejocahn@gmaily.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(9, 'Andres', 'Guzman', 'profesacor@bolsta.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(10, 'Frankie', 'De John', 'dejacohn@gmaril.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(11, 'Andres', 'Guzman', 'profeacsor@bolwsa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(12, 'Frankie', 'De John', 'dejoachn@gmawil.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(13, 'Andres', 'Guzman', 'profesoacr@bolwwsa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(14, 'Frankie', 'De John', 'dejeohn@gmawil.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(15, 'Andres', 'Guzman', 'profewsor@boalsa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(16, 'Frankie', 'De John', 'dejohn@gmaail.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(17, 'Andres', 'Guzman', 'proqfesor@bolusa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(18, 'Frankie', 'De John', 'deejohn@gmyail.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(19, 'Andres', 'Guzman', 'proefesor@btolsa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(20, 'Frankie', 'De John', 'dejohn@gtmail.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(21, 'Andres', 'Guzman', 'profdesor@bolsa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(22, 'Frankie', 'De John', 'dejaotthn@gmail.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(23, 'Andres', 'Guzman', 'profeshaasor@bolsa.com', '2017-08-28', '');
+INSERT INTO clientes(id, nombre, apellido, email, create_at, foto) VALUES(24, 'Frankie', 'De John', 'dejyohn@gmail.com', '2017-08-28', '');
